aa1765f35b2e93e508a28f4465771b471ef8b334
ColniIIIko <isavitskiy12@gmail.com>
2023-04-03 00:15:21 +0300

    fix: fix countdown stopping when changing option

--- src/hooks/useCountDown.ts
+++ src/hooks/useCountDown.ts
@@ -4,14 +4,16 @@ export const useCountDown = (startValue: number, action: () => void) => {
   const [counter, setCounter] = useState(startValue);
 
   useEffect(() => {
-    if (!counter) {
-      action();
-      return;
-    }
     const id = setTimeout(() => {
       setCounter((prev) => prev - 1);
     }, 1000);
     return () => clearTimeout(id);
+  }, [counter]);
+
+  useEffect(() => {
+    if (counter === 0) {
+      action();
+    }
   }, [counter, action]);
 
   return counter;
