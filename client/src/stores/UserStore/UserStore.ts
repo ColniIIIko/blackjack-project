7bceae264b3e431f50e54395a9175faacfc8b4f5
ColniIIIko <isavitskiy12@gmail.com>
2023-04-19 14:04:06 +0300

    feat: add random username generation

--- client/src/stores/UserStore/UserStore.ts
+++ client/src/stores/UserStore/UserStore.ts
@@ -2,10 +2,11 @@ import { makeAutoObservable } from 'mobx';
 import { Bet, User } from '../../types/general';
 import { v4 as uuid } from 'uuid';
 import { createContext } from 'react';
+import { generateUsername } from 'unique-username-generator';
 
 class UserStore implements User {
   id: string = uuid();
-  name: string = 'anonymous';
+  name: string = generateUsername('', 0, Math.round(Math.random() * 16) + 1);
   balance: number = 1000;
   previousBet: Bet | null = null;
 
