b99fbbb71d8ca3dc09b3ca52fd74f01f5f2d95a2
ColniIIIko <isavitskiy12@gmail.com>
2023-04-24 15:39:32 +0300

    ref: adapte logo to mobile devices

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import UserInfo from '../UserInfo/UserInfo';
 
 import styles from './header.module.css';
-import { LOGO_URL } from '../../const';
 
 function Header() {
   return (
     <header className={styles['header']}>
-      <img
-        src={LOGO_URL.href}
-        alt='logo'
-      />
+      <div className={styles['logo']}></div>
       <UserInfo />
     </header>
   );
