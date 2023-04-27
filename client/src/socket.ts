f951ed116e193216d94831ba974ef33315a74f88
ColniIIIko <isavitskiy12@gmail.com>
2023-04-27 21:33:10 +0300

    deploy: testing 5

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,4 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 import { ClientToServerEvents, ServerToClientEvents } from './types/socket';
 
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('http://localhost:3000');
+const socketUrl = import.meta.env.DEV ? 'http://localhost:3000' : 'https://blackjack-back.onrender.com';
+
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(socketUrl);
